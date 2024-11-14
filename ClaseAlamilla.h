ad9441bfb577d8a7f0327a476e73d22e048a1e50
Sergio Hernández <sergiohernandez@tec.mx>
2024-11-13 23:34:07 -0600

    Tercer commit

---
+++ ClaseAlamilla.h
@@ -0,0 +1,16 @@
+//
+// Created by Sergio Francisco Hernández Alamilla on 13/11/24.
+//
+
+#ifndef CLASEALAMILLA_H
+#define CLASEALAMILLA_H
+
+
+
+class ClaseAlamilla {
+
+};
+
+
+
+#endif //CLASEALAMILLA_H
