66c336b0f9da285f8ed3a1d719dc4cd85bf43840
Sergio Hernández <sergiohernandez@tec.mx>
2024-11-13 22:58:06 -0600

    Primer commit

---
+++ TestClass.h
@@ -0,0 +1,16 @@
+//
+// Created by Sergio Francisco Hernández Alamilla on 13/11/24.
+//
+
+#ifndef TESTCLASS_H
+#define TESTCLASS_H
+
+
+
+class TestClass {
+
+};
+
+
+
+#endif //TESTCLASS_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "TestClass.h"
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    TestClass test;
+    return 0;
+}
